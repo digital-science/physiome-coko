727a6ded640cde4d68bfc333741029cf5e205c65
Jared Watts <j.watts@digital-science.com>
2019-06-17 10:55:31 +0100

    [Task Form] funding autocomplete, add data source notice

--- packages/component-task-form/client/components/funding-autocomplete.js
+++ packages/component-task-form/client/components/funding-autocomplete.js
@@ -3,7 +3,9 @@ import styled from 'styled-components';
 
 import useGetGrantsForProjectNumber from '../queries/getGrantsForProjectNumber';
 
-import { EntityAutocomplete, MenuItem, ApplySmallAutocompleteStyle } from "ds-awards-theme/components/entity-autocomplete";
+import { EntityAutocomplete, MenuItem, MenuHolder, ApplySmallAutocompleteStyle } from "ds-awards-theme/components/entity-autocomplete";
+import DimensionsLogo from "../static/dimensions-logo.svg";
+
 
 const grantEntityModifier = (entity) => {
     return entity;
@@ -26,10 +28,39 @@ const renderGrantValueRepresentation = (grantEntity) =>
 
 const getGrantItemValue = (grant) => grant.projectNumber;
 
+const LookupProviderNote = styled(MenuItem)`
+
+  font-size: 10px;
+  text-align: right;
+
+  & img {
+    height: 1.2em;
+    vertical-align: middle;
+  }  
+`;
+
+const FundingMenuHolder = styled(MenuHolder)`
+  & > * + ${LookupProviderNote} {
+    margin-top: 2px;
+    border-top: 1px dashed lightgray;
+  }
+`;
+
+const renderFundingMenu = function (items, value, style) {
+    return (
+        <FundingMenuHolder style={{ ...style, ...this.menuStyle }} children={items}>
+            {items}
+            <LookupProviderNote>
+                Grant details sourced via <img src={DimensionsLogo} alt="Dimensions logo" />
+            </LookupProviderNote>
+        </FundingMenuHolder>
+    );
+};
+
 
 const FundingAutocomplete = ({entityModifier=grantEntityModifier, entityLookup, getItemValue=getGrantItemValue,
                               renderItem=renderGrantMenuItem, renderEntityValueRepresentation=renderGrantValueRepresentation,
-                              ...props}) => {
+                              renderMenu=renderFundingMenu, ...props}) => {
 
     const [lookupGrantsForProjectNumber] = useGetGrantsForProjectNumber();
     const grantLookup = (value, maxItems=15) => {
@@ -38,7 +69,7 @@ const FundingAutocomplete = ({entityModifier=grantEntityModifier, entityLookup, 
 
     return (
         <EntityAutocomplete entityModifier={entityModifier} getItemValue={getItemValue} entityLookup={entityLookup || grantLookup}
-            renderItem={renderItem} renderEntityValueRepresentation={renderEntityValueRepresentation} {...props} />
+            renderItem={renderItem} renderEntityValueRepresentation={renderEntityValueRepresentation} renderMenu={renderMenu} {...props} />
     );
 };
 
