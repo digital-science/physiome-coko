d925c843636a16da1821860822550edfedb72f4b
Jared Watts <j.watts@digital-science.com>
2019-06-21 16:10:51 +0100

    [Task Form] keywords - add listing (read only) variation

--- packages/component-task-form/client/components/fields/form-field-keywords.js
+++ packages/component-task-form/client/components/fields/form-field-keywords.js
@@ -6,7 +6,10 @@ import styled from 'styled-components';
 import Label from 'ds-awards-theme/components/label';
 import TagInput from 'ds-awards-theme/components/tag-input';
 
-function _FormFieldKeywords({data, binding, options = {}}) {
+import { th } from 'ds-awards-theme';
+
+
+function _FormFieldKeywordsEditor({data, binding, options = {}}) {
 
     const [value, setValue] = useFormValueBinding(data, binding, []);
 
@@ -18,12 +21,61 @@ function _FormFieldKeywords({data, binding, options = {}}) {
     );
 }
 
-const FormFieldKeywords = styled(withFormField(_FormFieldKeywords))`
+const FormFieldKeywordsEditor = styled(withFormField(_FormFieldKeywordsEditor))`
+`;
+
+
+// ----
+// Listing (view only)
+// ----
+
+function _FormFieldKeywordsListing({className, data, binding, options = {}}) {
+
+    const [value] = useFormValueBinding(data, binding, []);
+
+    return (
+        <div className={className}>
+            {options.label ? <Label>{options.label}</Label> : null}
+            {value && value instanceof Array && value.length ? (
+                <ol>
+                    {value.map((tag, index) =>
+                        <li key={index}>{tag}</li>
+                    )}
+                </ol>
+            ) : <div className="empty">No keywords supplied</div>
+            }
+        </div>
+    );
+}
+
+const FormFieldKeywordsListing = styled(withFormField(_FormFieldKeywordsListing))`
+
+  font-family: ${th('tagInput.fontFamily')};
+  font-size: 14px;
+
+  & ol {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+  & ol li {
+    display: inline-block;
+    padding: 2px 5px;
+    border: 1px solid #505050;
+    background: #909090;
+    color: white;
+    border-radius: 5px;
+  }
+  div.empty {
+    color: #b3b3b3;
+  }
 `;
 
 
 
-/*
- */
 
-export default FormFieldKeywords;
+
+export default FormFieldKeywordsEditor;
+
+export { FormFieldKeywordsListing };
+
