6a569c7059174edc09a490c53efff95c1de264f6
Jared Watts <j.watts@digital-science.com>
2019-06-26 08:46:36 +0100

    [Task Form] text area - add initial support for applying validations and showing validation issues

--- packages/component-task-form/client/components/fields/form-field-textarea.js
+++ packages/component-task-form/client/components/fields/form-field-textarea.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 
 import { FormFieldInlineTaskHolder } from './form-field-inline-task';
 import useFormValueBinding from './../../hooks/useFormValueBinding';
+import useFormValidation from './../../hooks/useFormValidation';
 import withFormField from './withFormField'
 
 import Label from 'ds-awards-theme/components/label';
@@ -19,15 +20,26 @@ const FormStyledTextArea = styled(TextArea)`
 `;
 
 
-function FormFieldTextArea({data, binding, options = {}}) {
+function FormFieldTextArea({data, binding, description, formDefinition, formValidator, options = {}}) {
 
     const [value, _, handleInputChange] = useFormValueBinding(data, binding, "", (v) => v || "");
-    const textInput = <FormStyledTextArea value={value || ""} onChange={handleInputChange} rows={options.rows || 2} />;
+    const [validationWarnings, clearValidationWarnings] = useFormValidation(description, formDefinition, formValidator);
+
+    const handleInputChangeWithWarningsClear = (e) => {
+        clearValidationWarnings();
+        handleInputChange(e);
+    };
+
+    const textInput = (
+        <FormStyledTextArea value={value || ""} onChange={handleInputChangeWithWarningsClear}
+            rows={options.rows || 2} issue={validationWarnings && validationWarnings.length}  />
+    );
 
     return (
         <React.Fragment>
             {options.label ? <React.Fragment><Label>{options.label}</Label><br /></React.Fragment> : null}
             {textInput}
+            { validationWarnings ? validationWarnings.map((warning, index) => <div key={index}>{warning}</div>) : null }
         </React.Fragment>
     );
 }
