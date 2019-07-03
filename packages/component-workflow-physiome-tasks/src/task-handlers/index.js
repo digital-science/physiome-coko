093f054f80c0bf7f5d1b48c9e67f13408fbc9581
Jared Watts <j.watts@digital-science.com>
2019-07-03 14:59:39 +0100

    [Physiome Tasks] register reserve doi service

--- packages/component-workflow-physiome-tasks/src/task-handlers/index.js
+++ packages/component-workflow-physiome-tasks/src/task-handlers/index.js
@@ -3,12 +3,13 @@ const TaskEmailManuscriptAcceptance = require('./task-email-acceptance');
 const TaskEmailManuscriptRejection = require('./task-email-rejection');
 
 const TaskPublishArticle = require('./task-publish-article');
+const TaskReserveDoi = require('./task-reserve-doi');
 
 const TaskTimeoutSubmission = require('./task-time-out-submission');
 
 
 const AllTaskSetups = [TaskEmailInitialSubmission, TaskEmailManuscriptAcceptance, TaskEmailManuscriptRejection,
-    TaskPublishArticle, TaskTimeoutSubmission];
+    TaskPublishArticle, TaskReserveDoi, TaskTimeoutSubmission];
 
 
 module.exports = function initExternalTasks(client) {
