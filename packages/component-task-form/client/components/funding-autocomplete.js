87be21b4350fa50ab7b15d469c8e077a21c7c5e7
Jared Watts <j.watts@digital-science.com>
2019-07-15 17:46:57 +0100

    [Task Form] funding autocomplete - remove old code from previous version using react-autocomplete

--- packages/component-task-form/client/components/funding-autocomplete.js
+++ packages/component-task-form/client/components/funding-autocomplete.js
@@ -2,94 +2,14 @@ import React from 'react';
 import styled from 'styled-components';
 
 import useGetGrantsForProjectNumber from 'dimensions-lookup-service/client/queries/getGrantsForProjectNumber';
-import Spinner from "ds-awards-theme/components/spinner";
-import { th } from "ds-awards-theme";
-
-//import { EntityAutocomplete, MenuItem, MenuHolder, ApplySmallAutocompleteStyle } from "ds-awards-theme/components/entity-autocomplete";
-import DimensionsLogo from "../static/dimensions-logo.svg";
-
-/*
-const grantEntityModifier = (entity) => {
-    return entity;
-};
-
-const FunderMenuItem = styled(MenuItem)`
-`;
-
-
-const renderGrantMenuItem = (item, isHighlighted) =>
-    <FunderMenuItem key={item.id} className={isHighlighted ? "selected" : ""}>
-        {item.projectNumber} &mdash; {item.title}
-    </FunderMenuItem>;
-
-
-const renderGrantValueRepresentation = (grantEntity) =>
-    <React.Fragment>
-        {grantEntity.projectNumber}
-    </React.Fragment>;
-
-const getGrantItemValue = (grant) => grant.projectNumber;
-
-const LookupProviderNote = styled(MenuItem)`
-
-  font-size: 10px;
-  text-align: right;
-
-  & img {
-    height: 1.2em;
-    vertical-align: middle;
-  }  
-`;
-
-const FundingMenuHolder = styled(MenuHolder)`
-  & > * + ${LookupProviderNote} {
-    margin-top: 2px;
-    border-top: 1px dashed lightgray;
-  }
-`;
-
-const renderFundingMenu = function (items, value, style) {
-    return (
-        <FundingMenuHolder style={{ ...style, ...this.menuStyle }} children={items}>
-            {items}
-            <LookupProviderNote>
-                Grant details sourced via <img src={DimensionsLogo} alt="Dimensions logo" />
-            </LookupProviderNote>
-        </FundingMenuHolder>
-    );
-};
-
-
-const FundingAutocomplete = ({entityModifier=grantEntityModifier, entityLookup, getItemValue=getGrantItemValue,
-                              renderItem=renderGrantMenuItem, renderEntityValueRepresentation=renderGrantValueRepresentation,
-                              renderMenu=renderFundingMenu, ...props}) => {
-
-    const [lookupGrantsForProjectNumber] = useGetGrantsForProjectNumber();
-    const grantLookup = (value, maxItems=15) => {
-        if(!value || !value.length) {
-            return Promise.resolve([]);
-        }
-        return lookupGrantsForProjectNumber(value).catch(e => []);
-    };
-
-    return (
-        <EntityAutocomplete entityModifier={entityModifier} getItemValue={getItemValue} entityLookup={entityLookup || grantLookup}
-            renderItem={renderItem} renderEntityValueRepresentation={renderEntityValueRepresentation} renderMenu={renderMenu} {...props} />
-    );
-};
-
-export default FundingAutocomplete;
-
-const SmallFundingAutocomplete = ApplySmallAutocompleteStyle(FundingAutocomplete);
-
-export { SmallFundingAutocomplete };
-
-*/
-
-
 
 import { AutocompleteEntity, SmallAutocompleteEntity } from 'ds-awards-theme/components/autocomplete-entity';
 import { MenuHolder, MenuHeader, MenuFooter } from 'ds-awards-theme/components/autocomplete';
+import Spinner from "ds-awards-theme/components/spinner";
+import { th } from "ds-awards-theme";
+
+import DimensionsLogo from "../static/dimensions-logo.svg";
+
 
 const LookupProviderHeader = styled(MenuHeader)`
 `;
@@ -134,12 +54,6 @@ const renderGrantMenuFooter = (loading) => (
         <div>Grant details sourced via <img src={DimensionsLogo} alt="Dimensions logo" /></div>
     </LookupProviderNote>
 );
-
-
-const renderGrantValueRepresentation = (grantEntity) =>
-    <React.Fragment>
-        {grantEntity.projectNumber}
-    </React.Fragment>;
 
 const getGrantItemValue = (grant) => grant.projectNumber;
 
