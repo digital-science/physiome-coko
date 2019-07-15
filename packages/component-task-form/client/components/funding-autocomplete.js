29a844e235135bcac56daec309f27042cb7ff4dc
Jared Watts <j.watts@digital-science.com>
2019-07-15 17:29:30 +0100

    [Task Form] funding autocomplete - shift to new autocomplete component

--- packages/component-task-form/client/components/funding-autocomplete.js
+++ packages/component-task-form/client/components/funding-autocomplete.js
@@ -2,11 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 import useGetGrantsForProjectNumber from 'dimensions-lookup-service/client/queries/getGrantsForProjectNumber';
+import Spinner from "ds-awards-theme/components/spinner";
+import { th } from "ds-awards-theme";
 
-import { EntityAutocomplete, MenuItem, MenuHolder, ApplySmallAutocompleteStyle } from "ds-awards-theme/components/entity-autocomplete";
+//import { EntityAutocomplete, MenuItem, MenuHolder, ApplySmallAutocompleteStyle } from "ds-awards-theme/components/entity-autocomplete";
 import DimensionsLogo from "../static/dimensions-logo.svg";
 
-
+/*
 const grantEntityModifier = (entity) => {
     return entity;
 };
@@ -81,3 +83,95 @@ export default FundingAutocomplete;
 const SmallFundingAutocomplete = ApplySmallAutocompleteStyle(FundingAutocomplete);
 
 export { SmallFundingAutocomplete };
+
+*/
+
+
+
+import { AutocompleteEntity, SmallAutocompleteEntity } from 'ds-awards-theme/components/autocomplete-entity';
+import { MenuHolder, MenuHeader, MenuFooter } from 'ds-awards-theme/components/autocomplete';
+
+const LookupProviderHeader = styled(MenuHeader)`
+`;
+
+const LookupProviderNote = styled(MenuFooter)`
+  font-family: ${th('autocomplete.item.fontFamily')};
+  font-size: 10px;
+  text-align: right;
+
+  & img {
+    height: 1.2em;
+    vertical-align: middle;
+  }  
+`;
+
+const FundingMenuHolder = styled(MenuHolder)`  
+  & > ${LookupProviderHeader} + * {
+    margin-top: 2px;
+    border-top: 1px dashed lightgray;
+  }
+  & > * + ${LookupProviderNote} {
+    margin-top: 2px;
+    border-top: 1px dashed lightgray;
+  }
+`;
+
+const grantEntityModifier = (entity) => entity;
+
+const renderGrantMenuItem = ({item, index, isSelected}, MenuItemComponent) =>
+    <MenuItemComponent key={item.id} className={isSelected ? "selected" : ""}>
+        {item.projectNumber} &mdash; {item.title}
+    </MenuItemComponent>;
+
+const renderGrantMenuHeader = (loading) => loading ? (
+    <LookupProviderHeader>
+        <Spinner message="Loading…" small={true} />
+    </LookupProviderHeader>
+) : null;
+
+const renderGrantMenuFooter = (loading) => (
+    <LookupProviderNote>
+        <div>Grant details sourced via <img src={DimensionsLogo} alt="Dimensions logo" /></div>
+    </LookupProviderNote>
+);
+
+
+const renderGrantValueRepresentation = (grantEntity) =>
+    <React.Fragment>
+        {grantEntity.projectNumber}
+    </React.Fragment>;
+
+const getGrantItemValue = (grant) => grant.projectNumber;
+
+//renderEntityValueRepresentation=renderGrantValueRepresentation
+
+const FundingAutocomplete = ({entityModifier=grantEntityModifier, lookupItems, getItemValue=getGrantItemValue, menuHolderComponent=FundingMenuHolder,
+                              renderMenuItem=renderGrantMenuItem, renderMenuHeader=renderGrantMenuHeader,
+                              renderMenuFooter=renderGrantMenuFooter, autocompleteEntityComponent=AutocompleteEntity, ...props}) => {
+
+    const [lookupGrantsForProjectNumber] = useGetGrantsForProjectNumber();
+    const grantLookup = (value, maxItems=15) => {
+        if(!value || !value.length) {
+            return Promise.resolve([]);
+        }
+        return lookupGrantsForProjectNumber(value).catch(e => []);
+    };
+
+    const AutocompleteEntityComponent = autocompleteEntityComponent;
+
+    return (
+        <AutocompleteEntityComponent entityModifier={entityModifier} getItemValue={getItemValue} lookupItems={lookupItems || grantLookup} menuHolderComponent={menuHolderComponent}
+            renderMenuItem={renderMenuItem} renderMenuHeader={renderMenuHeader} renderMenuFooter={renderMenuFooter} {...props} />
+    );
+};
+
+
+const SmallFundingAutocomplete = ({autocompleteEntityComponent=SmallAutocompleteEntity, ...rest}) => {
+
+    return <FundingAutocomplete autocompleteEntityComponent={autocompleteEntityComponent} {...rest} />;
+};
+
+
+export default FundingAutocomplete;
+export { FundingAutocomplete, SmallFundingAutocomplete };
+
