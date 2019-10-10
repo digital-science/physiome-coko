0da4b3eb4ad8c3bd3e5cea1a887ff7fa5d44b587
Jared Watts <j.watts@digital-science.com>
2019-10-10 21:33:06 +1300

    [Physiome] update default dev config, fix user listing access ACL rule

--- packages/app/config/development.js
+++ packages/app/config/development.js
@@ -1,15 +1,25 @@
 const { deferConfig } = require('config/defer');
 
 module.exports = {
+
     'pubsweet-server': {
         baseUrl: deferConfig(
             cfg => `http://localhost:${cfg['pubsweet-server'].port}`,
         ),
     },
+
     dbManager: {
         username: 'admin',
         password: 'password',
         email: 'admin@example.com',
         admin: true,
+    },
+
+    identity: {
+        validationTokenExpireDays: 15,
+        maximumEmailValidationsPerDay: 5,
+        adminIdentities: [
+            "0000-0001-9332-2604"
+        ]
     }
 };
