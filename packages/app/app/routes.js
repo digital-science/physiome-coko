e95754e2605b378009e94055efc6b47ff524db1f
Jared Watts <j.watts@digital-science.com>
2019-10-11 08:06:17 +1300

    [App] change routes to show submitters submission page dashboard layout

--- packages/app/app/routes.js
+++ packages/app/app/routes.js
@@ -9,18 +9,20 @@ import {
     PageDashboardActiveSubmissions,
     PageDashboardPublishedSubmissions,
     PageDashboardRejectedSubmissions,
+    PageDashboardSubmitterSubmissions,
     PageSubmissionForm,
     PageSubmissionDetails
 } from 'physiome-ui/client';
 
 
 
-const renderAppDefault = (children, props={}) => <SubmissionApp {...props}>{children}</SubmissionApp>;
-const renderAppHideSidebar = (children, props={}) => <SubmissionApp hideSidebar={true} {...props}>{children}</SubmissionApp>;
+const renderAppDefault = (children, currentUser, props={}) => <SubmissionApp {...props}>{children}</SubmissionApp>;
+const renderAppHideSidebar = (children, currentUser, props={}) => <SubmissionApp hideSidebar={true} {...props}>{children}</SubmissionApp>;
 
 const Routes = () => (
 
     <Switch>
+
         <Route path="/submission/:instanceId" render={props=> {
             return (
                 <LoginRequiredRoute message="You must login before being able to finish submitting details of your in-progress submission. Login using your ORCID credentials." renderApplication={renderAppHideSidebar}
@@ -29,8 +31,16 @@ const Routes = () => (
         }} />
 
         <Route path="/details/:instanceId" render={props=> {
+
+            const renderAppDetails = (children, currentUser, p = {}) => {
+                if(currentUser.groups && currentUser.groups.indexOf("administrator") !== -1) {
+                    return renderAppDefault(children, currentUser, p);
+                }
+                return renderAppHideSidebar(children, currentUser, p);
+            };
+
             return (
-                <LoginRequiredRoute message="You must login to be able to view details of the requested submission. Login using your ORCID credentials." renderApplication={renderAppDefault}
+                <LoginRequiredRoute message="You must login to be able to view details of the requested submission. Login using your ORCID credentials." renderApplication={renderAppDetails}
                     renderContent={children => <PageSubmissionDetails children={children} {...props} />} />
             );
         }} />
@@ -52,11 +62,25 @@ const Routes = () => (
         }} />
 
         <Route path="/" render={props => {
-            return (
-                <LoginRequiredRoute message="To start a new submission please login using your ORCID ID." renderApplication={renderAppDefault}>
-                    <PageDashboardActiveSubmissions history={props.history} />
-                </LoginRequiredRoute>
-            );
+
+            const renderMainDashboard = (children, currentUser) => {
+
+                if(currentUser.groups && currentUser.groups.indexOf("administrator") !== -1) {
+                    return (
+                        <SubmissionApp>
+                            <PageDashboardActiveSubmissions history={props.history} />
+                        </SubmissionApp>
+                    );
+                }
+
+                return (
+                    <SubmissionApp hideSidebar={true}>
+                        <PageDashboardSubmitterSubmissions history={props.history} />
+                    </SubmissionApp>
+                );
+            };
+
+            return <LoginRequiredRoute message="To start a new submission please login using your ORCID ID." renderApplication={renderMainDashboard} />;
         }} />
 
     </Switch>
