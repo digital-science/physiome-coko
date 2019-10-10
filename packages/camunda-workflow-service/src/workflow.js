d834d5faa39ab52ca31e0c459efe8cce834b9b3c
Jared Watts <j.watts@digital-science.com>
2019-10-10 15:25:37 +1300

    [Camunda Workflow Service] set default apiUri if no config available

--- packages/camunda-workflow-service/src/workflow.js
+++ packages/camunda-workflow-service/src/workflow.js
@@ -3,7 +3,7 @@ const config = require('config');
 
 const camClient = new CamSDK.Client({
     mock: false,
-    apiUri: config.workflow.apiUri
+    apiUri: (config && config.workflow) ? config.workflow.apiUri : `http://127.0.0.1:8080/engine-rest`
 });
 
 exports.client = camClient;
