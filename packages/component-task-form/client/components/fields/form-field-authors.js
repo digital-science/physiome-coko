87b7337ca6443a0121246be5da91f0f18d74116c
Jared Watts <j.watts@digital-science.com>
2019-11-10 19:24:22 +1300

    [Task Form] authors listing, show additional details, including relationships and ORCID ID

--- packages/component-task-form/client/components/fields/form-field-authors.js
+++ packages/component-task-form/client/components/fields/form-field-authors.js
@@ -175,12 +175,47 @@ export default withFormField(FormFieldAuthorsEditor);
 const AuthorRow = styled.li`
   > div {
     margin-left: 24px;
+    padding-top: 5px;
     font-size: 95%;
     color: #505050;
   }
   
+  > div > img {
+    vertical-align: bottom;
+  }
+  
   > ul {
     font-size: 95%;
+  }
+  
+  > ul.details {
+     display: block;
+     list-style: none;
+     padding: 0.25em 0 0.25em 0;
+     margin-left: 24px;
+         
+     li {
+       display: inline;
+     }
+     
+     li span {
+       display: inline-block;
+       /*color: white;*/
+       /*font-family: SFCompactDisplayRegular,sans-serif;
+       font-size: 12px;*/
+       /*background-color: #98cff1;
+       padding: 3px 8px;
+       border-radius: 3px;*/
+     }
+     
+     li:after {
+       content: ", ";
+     }
+     
+     li:last-child:after {
+        content: "";
+     }
+
   }
 `;
 
@@ -190,10 +225,38 @@ const AuthorListing = styled( ({className, authors}) => {
             {authors.map((author, index) => {
 
                 const affiliations = author.affiliations ? author.affiliations.filter(a => a.organization && a.organization.name) : null;
+                const details = [];
+
+                if(author.isCorresponding) {
+                    details.push(`Corresponding Author`);
+                }
+
+                if(author.didDevelopModel) {
+                    details.push(`Contributed to Reproducible Model`);
+                }
+
+                if(author.isPrimaryPaperAuthor) {
+                    details.push(`Author on Primary Paper`);
+                }
 
                 return (
                     <AuthorRow key={index}>
-                        {index + 1}. {author.name} ({author.email})
+                        {index + 1}. {author.name} - {author.email}
+                        {author.orcid ?
+                            <Fragment>
+                                <div>ORCID:&nbsp;
+                                    <img src={'https://orcid.org/sites/default/files/images/orcid_16x16.gif'} alt={'ORCID icon'} />&nbsp;
+                                    <a href={`https://orcid.org/${encodeURI(author.orcid)}`} className={'orcid'} target="_blank" rel="noopener noreferrer">https://orcid.org/{author.orcid}</a>
+                                </div>
+                            </Fragment> : null
+                        }
+                        {details.length ?
+                            <Fragment>
+                                <div>Relationships:</div>
+                                <ul className={'details'}>
+                                    {details.map((d, index) => <li key={index}><span>{d}</span></li>)}
+                                </ul>
+                            </Fragment> : null}
                         { affiliations ?
                             <Fragment>
                                 <div>Affiliations:</div>
