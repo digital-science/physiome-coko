53baa23eb3dbf0ac19503defca074c916ab6742b
Jared Watts <j.watts@digital-science.com>
2019-11-26 21:42:37 +1300

    [Physiome UI] submission form page - add navigation prevention with warning message when navigating away from in-progress submission

--- packages/physiome-ui/client/pages/submission-form.js
+++ packages/physiome-ui/client/pages/submission-form.js
@@ -1,6 +1,10 @@
-import React, { useContext } from 'react';
-import { WorkflowDescriptionContext } from 'client-workflow-model'
-import { taskFormForFormDefinition } from 'component-task-form/client'
+import React, { useContext, useState, useCallback } from 'react';
+import { Prompt } from 'react-router-dom';
+
+import { WorkflowDescriptionContext } from 'client-workflow-model';
+import { taskFormForFormDefinition } from 'component-task-form/client';
+
+import { BasicMessage, BasicMessageButton } from 'component-overlay';
 import styled from 'styled-components';
 
 const SubmissionDefaultType = 'submission';
@@ -12,6 +16,10 @@ function PageSubmissionForm({ match, history, children }) {
     const { instanceId, taskId, taskName = SubmissionDefaultTaskName, type = SubmissionDefaultType } = match.params;
     const workflowDescription = useContext(WorkflowDescriptionContext);
 
+    const [didSubmit, setDidSubmit] = useState(false);
+    const [showUnsubmittedSubmissionMessage, setShowUnsubmittedSubmissionMessage] = useState(false);
+    const [prevLocation, setPreviousLocation] = useState(false);
+
     const instanceType = workflowDescription.findInstanceTypeForUrlName(type);
     if(!instanceType) {
         throw new Error(`Unable to find task definition for type '${type}'.`);
@@ -21,14 +29,61 @@ function PageSubmissionForm({ match, history, children }) {
     const TaskFormType = taskFormForFormDefinition(formDefinition);
 
     const wasSubmitted = () => {
-        history.push("/");
+        setDidSubmit(true);
+        setTimeout(() => {
+            history.push("/");
+        }, 0);
         return Promise.resolve();
     };
+
+    const blockNavigation = useCallback((nextLocation) => {
+
+        if(didSubmit) {
+            return true;
+        }
+
+        console.log(`did block`);
+        console.dir(nextLocation);
+
+        setPreviousLocation(nextLocation);
+        setShowUnsubmittedSubmissionMessage(true);
+        return false;
+
+    }, [didSubmit, setPreviousLocation]);
+
+    const hideUnsubmittedSubmissionMessage = useCallback(() => {
+
+        setPreviousLocation(null);
+        setShowUnsubmittedSubmissionMessage(false);
+
+    }, [setPreviousLocation, setShowUnsubmittedSubmissionMessage]);
+
+    const discardUnsubmittedSubmission = useCallback(() => {
+
+        setDidSubmit(true);
+        setTimeout(() => {
+            history.push(prevLocation);
+        }, 0);
+
+    }, [history, prevLocation, setDidSubmit]);
+
 
     const heading = formDefinition && formDefinition.options ? formDefinition.options.heading : null;
 
     return (
         <SubmissionFormHolder>
+            <Prompt when={!didSubmit} message={blockNavigation} />
+            <BasicMessage isOpen={showUnsubmittedSubmissionMessage} closeOverlay={hideUnsubmittedSubmissionMessage} heading="In-progress Submission"
+                rawMessage={'Your in-progress submission has not been saved or submitted. Please click \'Continue\' to make further changes to your submission, submit or save the submission. Clicking \'Leave\' will navigate away from the submission discarding it.<br /><br />If you want to come back to this submission at a later date, click \'Continue\' and then select the \'Save for Later\' option at the bottom of the page.'}
+                buttons={
+                    <MessageButtonsHolder>
+                        <BasicMessageButton onClick={discardUnsubmittedSubmission}>Leave</BasicMessageButton>
+                        <BasicMessageButton onClick={hideUnsubmittedSubmissionMessage}>Continue</BasicMessageButton>
+                    </MessageButtonsHolder>
+                }
+            />
+
+
             <SubmissionFormPageHeadingHolder className="message-holder">
                 {children}
             </SubmissionFormPageHeadingHolder>
@@ -55,6 +110,15 @@ const SubmissionFormHolder = styled.div`
   
   & > ${SubmissionFormHolder}:first-child {
       border-top: 2px solid #ebebeb;
+  }
+`;
+
+const MessageButtonsHolder = styled.div`
+  & > button {
+    min-width: 6em;
+  }
+  & > button + button {
+    margin-left: 10px;
   }
 `;
 
